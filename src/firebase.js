f823c255ec516e9ad2ad4a1dea92a474b7f0768e
Xander Salathe <xsalathe@gmail.com>
2024-10-05 16:32:47 +1000

    feat: firebase init, arr for flash card data

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB0_vkkY278coPFxB15933CDkpSxlny5_A",
+    authDomain: "quizquokka-5f7fa.firebaseapp.com",
+    projectId: "quizquokka-5f7fa",
+    storageBucket: "quizquokka-5f7fa.appspot.com",
+    messagingSenderId: "377588266099",
+    appId: "1:377588266099:web:7a707acf64d8873d126606",
+    measurementId: "G-T7RF1SN8KC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
