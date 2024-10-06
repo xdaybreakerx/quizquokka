81ff21c5426ea93f08cd4d4769319de7d180be28
Xander Salathe <xsalathe@gmail.com>
2024-10-06 18:45:10 +1100

    feat: flash card flip for question / answer

--- src/components/FlashCard/FlashCard.jsx
+++ src/components/FlashCard/FlashCard.jsx
@@ -1,29 +1,41 @@
-export default function FlashCard() {    
+import { useState } from "react";
+import "./FlashCard.css";
+
+export default function FlashCard() {
+  const [isRotated, setIsRotated] = useState(false);
+
+  const toggleCard = () => {
+    setIsRotated(!isRotated);
+  };
+
   return (
-    <div id="flash-card">
-      <div id="flash-card-question">
-        <h1>Question</h1>
-        <p id="quesiton-body">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi maiores
-          sunt optio, voluptate iste saepe. Praesentium quasi distinctio illum
-          laboriosam? Pariatur perspiciatis at voluptatibus commodi accusantium
-          eaque quod minus explicabo?
-        </p>
-        <button>Show Answer</button>
-      </div>
-      <div id="flash-card-answer">
-        <h1>Answer</h1>
-        <p id="answer-body">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur
-          iste voluptate deleniti quod neque omnis accusamus nostrum iure dicta
-          ex ratione voluptatem aperiam modi itaque dolorem, vel fugit aliquam
-          placeat.
-        </p>
-        <div>
-          <button>Explain Answer?</button>
-          <button>Show Question</button>
+    <div className="flash-card">
+      <div className={`flash-card-inner ${isRotated ? "rotate" : ""}`}>
+        <div className="flash-card-question">
+          <h1>Question</h1>
+          <p className="question-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+            maiores sunt optio, voluptate iste saepe. Praesentium quasi
+            distinctio illum laboriosam? Pariatur perspiciatis at voluptatibus
+            commodi accusantium eaque quod minus explicabo?
+          </p>
+        </div>
+        <div className="flash-card-answer">
+          <h1>Answer</h1>
+          <p className="answer-body">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consectetur iste voluptate deleniti quod neque omnis accusamus
+            nostrum iure dicta ex ratione voluptatem aperiam modi itaque
+            dolorem, vel fugit aliquam placeat.
+          </p>
+          <div>
+            <button>Explain Answer?</button>
+          </div>
         </div>
       </div>
+      <button onClick={toggleCard}>
+        {isRotated ? "Show Question" : "Show Answer"}
+      </button>
     </div>
   );
 }
