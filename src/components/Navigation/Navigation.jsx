92d7ea3981a574ca64886cef01fe2233b13ebfc5
Xander Salathe <xsalathe@gmail.com>
2024-10-08 16:56:44 +1100

    fix: navigation rewrite to use react-router-dom not next.js

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,13 +1,13 @@
-// module imports
-import * as React from "react";
+import React from "react";
+
 import { Link } from "react-router-dom";
+
 import { cn } from "@/lib/utils";
 
 import {
   NavigationMenu,
   NavigationMenuContent,
   NavigationMenuItem,
-  NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
@@ -53,10 +53,8 @@ export default function Navigation() {
           {/* Render the "Home" navigation item */}
           {navigationItems.map((item) => (
             <NavigationMenuItem key={item.title}>
-              <Link to={item.href} legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                  {item.title}
-                </NavigationMenuLink>
+              <Link to={item.href} className={navigationMenuTriggerStyle()}>
+                {item.title}
               </Link>
             </NavigationMenuItem>
           ))}
@@ -86,24 +84,23 @@ export default function Navigation() {
 
 // ListItem Component
 const ListItem = React.forwardRef(
-  ({ className, title, children, ...props }, ref) => {
+  ({ className, title, children, href, ...props }, ref) => {
     return (
       <li>
-        <NavigationMenuLink asChild>
-          <a
-            ref={ref}
-            className={cn(
-              "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-              className
-            )}
-            {...props}
-          >
-            <div className="text-sm font-medium leading-none">{title}</div>
-            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-              {children}
-            </p>
-          </a>
-        </NavigationMenuLink>
+        <Link
+          ref={ref}
+          to={href}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </Link>
       </li>
     );
   }
