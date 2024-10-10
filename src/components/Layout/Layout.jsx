854082fa718bde7a13aa10783616e348f7cf95de
Xander Salathe <xsalathe@gmail.com>
2024-10-10 17:29:03 +1100

    fix: apply background and text color variables to all components

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,7 +4,7 @@ import Footer from "@/components/Footer/Footer";
 
 export default function Layout({ children }) {
   return (
-    <div className="flex flex-col min-h-svh mx-auto" id="layout">
+    <div className="flex flex-col min-h-svh mx-auto bg-background text-foreground" id="layout">
       <Navigation />
       <main className="flex-grow p-4 max-w-80ch" id="main-content">
         {children}
