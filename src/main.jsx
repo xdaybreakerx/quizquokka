1abb46748c2498cdbaa79785d9cb85c83e5071e5
Xander Salathe <xsalathe@gmail.com>
2024-10-11 20:53:29 +1100

    feat: auth context wrapper around app

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App.jsx";
 import { ThemeProvider } from "./contexts/ThemeContextProvider.jsx";
+import { AuthContextProvider } from "./contexts/AuthContextProvider.jsx";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </ThemeProvider>
   </StrictMode>
 );
